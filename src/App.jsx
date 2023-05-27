c8f04464387565b79eb2283ced584a0ad5a2886a
Sai Surya Teja <saisuryateja3@gmail.com>
2023-05-28 01:33:36 +0530

    Radio Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import MuiSelect from "./components/MuiSelect";
+import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiSelect from "./components/MuiSelect";
 // import MuiTextField from "./components/MuiTextField";
 // import MuiButton from './components/MuiButton'
 // import TypographyPractice from './components/TypographyPractice'
@@ -10,7 +11,8 @@ function App() {
       {/* <TypographyPractice/> */}
       {/* <MuiButton/> */}
       {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      <MuiRadioButton />
     </>
   );
 }
